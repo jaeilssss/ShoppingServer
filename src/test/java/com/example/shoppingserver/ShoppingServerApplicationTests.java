f4ec5eb325378e77584792def31365ad9fa387c8
jaeilssss <jaeilssss@gmail.com>
2024-06-27 21:46:06 +0900

    init

---
+++ src/test/java/com/example/shoppingserver/ShoppingServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.shoppingserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShoppingServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
