b8dd1f8e83a2072249bf3e92a7ce34335a5a4bbe
jaeilssss <jaeilssss@gmail.com>
2024-07-02 22:58:23 +0900

    #3 각종 변경 파일 커밋

--- src/main/java/com/example/shoppingserver/ShoppingServerApplication.java
+++ src/main/java/com/example/shoppingserver/ShoppingServerApplication.java
@@ -2,7 +2,11 @@ package com.example.shoppingserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
+@EnableJpaAuditing
+@EnableJpaRepositories(basePackages = {"com.example.shoppingserver"})
 @SpringBootApplication
 public class ShoppingServerApplication {
 
